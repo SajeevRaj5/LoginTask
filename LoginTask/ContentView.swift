25a986b688667fe8e643d2ca419df8ba55af7d5d
sajeev <sajeevraj.1989@gmail.com>
2021-12-09 20:33:18 +0100

    Initial Commit

---
+++ LoginTask/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginTask
+//
+//  Created by Sajeev Raj on 09/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
